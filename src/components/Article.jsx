7b1a82972f0e10b0f2a7fdcdd4ecf122c1f8f201
gabriel-mockers-nws <gmockers@normandiewebschool.fr>
2025-07-24 17:30:33 +0200

    Modify PropTypes

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,11 +1,9 @@
-import proptypes from 'prop-types';
-import Button from './Button'; 
+import PropTypes from 'prop-types';
 
 export default function Article ({
     titre,
     reverse = false,
     images,
-    Button = [],
     children
 
 }) {
@@ -21,9 +19,16 @@ export default function Article ({
 
                 <div>
                     <h3 className="text-xl font-bold text-center md:text-left font-Marcellus">{titre}</h3>
+                    <div className='text-base leading-7 mb-5'>{children}</div>
                 </div>
-
             </div>
         </>
     )
+}
+
+Article.propTypes = {
+    titre: PropTypes.string.isRequired,
+    reverse: PropTypes.bool,
+    images: PropTypes.string.isRequired,
+    children: PropTypes.node.isRequired
 }
